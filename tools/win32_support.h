a8ef163b3e8469031da8c7b7a0c8ccea22e0aa66
Tyler Young <tyler.young@cimplx.com>
2016-06-29 03:49:43 -0400

    set default file mode to _O_BINARY on windows during initialization

--- tools/win32_support.h
+++ tools/win32_support.h
@@ -47,6 +47,33 @@
 #endif
 #endif
 
+#ifdef __cplusplus
+    #define INITIALIZER(f) \
+        static void f(void); \
+        struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+        static void f(void)
+#elif defined(_MSC_VER) && !defined(__GNUC__) && !defined(__MINGW32__) && !defined(__CYGWIN__)
+    #pragma section(".CRT$XCU",read)
+    #define INITIALIZER2_(f,p) \
+        static void f(void); \
+        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+        __pragma(comment(linker,"/include:" p #f "_")) \
+        static void f(void)
+    #ifdef _WIN64
+        #define INITIALIZER(f) INITIALIZER2_(f,"")
+    #else
+        #define INITIALIZER(f) INITIALIZER2_(f,"_")
+    #endif
+#else
+    #define INITIALIZER(f) \
+        static void f(void) __attribute__((constructor)); \
+        static void f(void)
+#endif
+
+INITIALIZER(win32_support) {
+	_set_fmode( _O_BINARY );
+}
+
 static inline FILE* ms_fopen(const char *filename, const char *mode) {
   FILE* result = 0;
   fopen_s(&result, filename, mode);
